82a60fdb220d74bad6770453cd3c372e43d4b0f2
Klurifixus <144474109+Klurifixus@users.noreply.github.com>
2023-12-07 11:10:20 +0100

    #7 setup path,content,styles to html,settings.py,static

--- earnsforum/earnsforum/settings.py
+++ earnsforum/earnsforum/settings.py
@@ -122,6 +122,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIR = [
+    BASE_DIR / "static", # Path: earnsforum/earnsforum/static
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
