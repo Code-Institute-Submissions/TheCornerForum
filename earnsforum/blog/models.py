971dc14ed8fc3e487241560732dffbce32be3586
Klurifixus <144474109+Klurifixus@users.noreply.github.com>
2023-12-08 12:29:49 +0100

    #9 setup many to many relations in models.py

--- earnsforum/blog/models.py
+++ earnsforum/blog/models.py
@@ -1,6 +1,16 @@
 from django.db import models
 
 
+class Tag(models.Model):
+    caption = models.CharField(max_length=20)
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email_address = models.EmailField()
+
+
 class Post(models.Model):
     title = models.CharField(max_length=175)
     excerpt = models.CharField(max_length=200, null=True, blank=True)
@@ -8,3 +18,6 @@ class Post(models.Model):
     date = models.DateField(auto_now=True)
     slug = models.SlugField(unique=True)
     content = models.TextField(max_length=10000, null=True, blank=True)
+    author = models.ForeignKey(
+        Author, on_delete=models.SET_NULL, null=True, blank=True, related_name='posts')
+    tags = models.ManyToManyField(Tag)
