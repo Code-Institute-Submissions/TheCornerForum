ff9a8a4ef7f53601fcb98a49a8e4f0ceaa6d7144
Klurifixus <144474109+Klurifixus@users.noreply.github.com>
2023-12-09 14:53:11 +0100

    #10 fixed the comments are now shown TESTING #8

--- earnsforum/blog/views.py
+++ earnsforum/blog/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from .models import Post
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView
 from .form import CommentForm
+from django.views import View
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 class StartingPageView(ListView):
     template_name = "blog/index.html"
@@ -27,13 +30,37 @@ def posts(request):
         "all_posts": all_posts
     })
 
-class SinglePostView(DetailView):
+class SinglePostView(View):
     template_name = "blog/post-detail.html"
     model = Post
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["posts_tags"] = self.object.tags.all()
-        context["comment_form"] = CommentForm()
-        return context
+    def get(self, request, slug):
+        post = Post.objects.get(slug=slug)
+        context = {
+            "post": post,
+            "post_tags": post.tags.all(),
+            "comment_form": CommentForm(),
+            "comments": post.comments.all().order_by('-id')
+        }
+        return render(request, "blog/post-detail.html", context)
+    
+    def post(self, request, slug):
+        comment_form = CommentForm(request.POST)
+        post = Post.objects.get(slug=slug)
 
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+            
+            return HttpResponseRedirect(reverse("post-detail-page", args=['slug']))
+        
+        context = {
+            "post": post,
+            "post_tags": post.tags.all(),
+            "comment_form": comment_form,
+            "comments": post.comments.all().order_by('-id')
+        }
+        return render(request, "blog/post-detail.html", context)
+
+        
