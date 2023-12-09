d6d527e7373070871eb956fb1fd3dd061a6ccd13
Klurifixus <144474109+Klurifixus@users.noreply.github.com>
2023-12-09 12:27:50 +0100

    #10 converting views to class based

--- earnsforum/blog/views.py
+++ earnsforum/blog/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.views.generic import ListView, DetailView
 
-def starting_page(request):
-    latest_posts = Post.objects.all().order_by('-date')[:3]
-    return render(request, "blog/index.html", {
-        "posts": latest_posts
-    })
+class StartingPageView(ListView):
+    template_name = "blog/index.html"
+    model = Post
+    ordering = ['-date']
+    context_object_name = "posts"
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        data = queryset[:3]
+        return data
+
+class AllPostsView(ListView):
+    template_name = "blog/all-posts.html"
+    model = Post
+    ordering = ['-date']
+    context_object_name = "all_posts"
 
 
 def posts(request):
@@ -14,11 +26,12 @@ def posts(request):
         "all_posts": all_posts
     })
 
+class SinglePostView(DetailView):
+    template_name = "blog/post-detail.html"
+    model = Post
 
-def post_detail(request, slug):
-    identified_post = get_object_or_404(Post, slug=slug)
-    return render(request, "blog/post-detail.html", {
-        "post": identified_post,
-        "post_tags": identified_post.tags.all()
-    })
-    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["posts_tags"] = self.object.tags.all()
+        return context
+
