bf73cf257e2c68f97d6b80d282bdf3d2c1b68fdc
Klurifixus <144474109+Klurifixus@users.noreply.github.com>
2023-12-11 16:37:47 +0100

    #14 is now working

--- earnsforum/blog/admin.py
+++ earnsforum/blog/admin.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 
 from .models import Author, Post, Tag, Comment
 
+
 class PostAdmin(admin.ModelAdmin):
     list_filter = ("author", "tags", "date",)
     list_display = ("title", "date", "author",)
     prepopulated_fields = {"slug": ("title",)}
+
 
 class CommentAdmin(admin.ModelAdmin):
     list_display = ("user_name", "post", "date",)
--- earnsforum/blog/models.py
+++ earnsforum/blog/models.py
@@ -15,10 +15,9 @@ class Author(models.Model):
 
     def full_name(self):
         return f"{self.first_name} {self.last_name}"
-    
+
     def __str__(self):
         return self.full_name()
-
 
 
 class Post(models.Model):
@@ -35,13 +34,14 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
+
 class Comment(models.Model):
     user_name = models.CharField(max_length=100)
-    user_email = models.EmailField( blank=True)
+    user_email = models.EmailField(blank=True)
     date = models.DateField(auto_now=True)
     text = models.TextField(max_length=2000)
     post = models.ForeignKey(
         Post, on_delete=models.CASCADE, related_name='comments')
-    
+
     def __str__(self):
         return f"{self.user_name} - {self.post.title}"
