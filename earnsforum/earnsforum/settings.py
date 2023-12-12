3d45b75e7a25f3438bd670deba20dba744b9f5d9
Klurifixus <144474109+Klurifixus@users.noreply.github.com>
2023-12-12 22:42:10 +0100

    Setting Up Heroku #22 test

--- earnsforum/earnsforum/settings.py
+++ earnsforum/earnsforum/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'default-secret-key-if-none-found')
 
 
-DEBUG = os.getenv('DEBUG', 'False') == 'True'
+DEBUG = False
 
 ALLOWED_HOSTS = [os.getenv("APP_HOST", '127.0.0.1')]
 
