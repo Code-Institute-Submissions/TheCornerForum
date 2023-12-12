41f270d453a2edb34eccc812dfbc0d520273362e
Klurifixus <144474109+Klurifixus@users.noreply.github.com>
2023-12-12 18:48:45 +0100

    #11 fixed database typos, paths for sqlite

--- earnsforum/earnsforum/settings.py
+++ earnsforum/earnsforum/settings.py
@@ -84,11 +84,14 @@ WSGI_APPLICATION = 'earnsforum.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
-        
-    }
+BASE_DIR = Path(__file__).resolve().parent.parent
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
