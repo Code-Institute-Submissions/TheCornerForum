e820bb271bda9a10adfc8bd7dfda20fa9176b30f
Klurifixus <144474109+Klurifixus@users.noreply.github.com>
2023-12-12 20:20:46 +0100

    #21 prepare for heroku

--- earnsforum/earnsforum/settings.py
+++ earnsforum/earnsforum/settings.py
@@ -88,8 +88,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'earnsdatabase',
+        'USER': 'admin',
+        'PASSWORD': 'admin007',
+        'HOST': 'localhost',
+        'PORT': '8000',
     }
 }
 
