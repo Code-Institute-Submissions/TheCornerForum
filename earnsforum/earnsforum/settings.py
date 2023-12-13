c20187d8ec4d7b7664dd87ccff5eeed28598695f
Klurifixus <144474109+Klurifixus@users.noreply.github.com>
2023-12-13 14:07:57 +0100

    #23 is fixed. when test #8. img is not upload to uploads

--- earnsforum/earnsforum/settings.py
+++ earnsforum/earnsforum/settings.py
@@ -121,8 +121,8 @@ STATICFILES_DIRS = [
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-MEDIA_ROOT = BASE_DIR / "uploads"  # Path: earnsforum/earnsforum/uploads
-MEDIA_URL = "/uploads/"            # URL: /uploads/
+MEDIA_ROOT = BASE_DIR / "uploads"
+MEDIA_URL = "/uploads/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
