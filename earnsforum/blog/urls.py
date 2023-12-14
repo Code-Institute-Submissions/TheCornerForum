c019855c2135fc7e6917ea1195cb75f5339242c4
Klurifixus <144474109+Klurifixus@users.noreply.github.com>
2023-12-14 19:54:50 +0100

    #29 updated views.py in blog with reverse, 404 control,validtest and comments

--- earnsforum/blog/urls.py
+++ earnsforum/blog/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     path("", views.StartingPageView.as_view(), name="starting-page"),
     path("posts/", views.AllPostsView.as_view(), name="posts-page"),
     path("posts/<slug:slug>", views.SinglePostView.as_view(), name="post-detail-page"),
-    #path("posts/<slug:slug>/comment/", views.AddCommentView.as_view(), name="add-comment"),
     path("read-later", views.ReadLaterView.as_view(), name="read-later"),
 
 ]
--- earnsforum/blog/views.py
+++ earnsforum/blog/views.py
@@ -6,9 +6,6 @@ from django.views import View
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from django.contrib.auth.mixins import LoginRequiredMixin
-
-
-
 from .models import Post
 from .form import CommentForm
 
@@ -20,35 +17,24 @@ class StartingPageView(ListView):
 
     def get_queryset(self):
         queryset = super().get_queryset()
-        data = queryset[:3]
+        data = queryset[:3]  # Display only the top 3 posts on the starting page
         return data
 
 class AllPostsView(ListView):
     template_name = "blog/all-posts.html"
     model = Post
     ordering = ['-date']
-    context_object_name = "all_posts"
-
-
-def posts(request):
-    all_posts = Post.objects.all().order_by('-date')
-    return render(request, "blog/all-posts.html", {
-        "all_posts": all_posts
-    })
+    context_object_name = "all_posts"  # Contains all posts for the 'all-posts' page
 
 class SinglePostView(View):
     def is_stored_posts(self, request, post_id):
+        # Logic to check if a post is in the user's 'read later' list
         stored_posts = request.session.get("stored_posts")
-        if stored_posts is  not None:
-            is_saved_for_later = post_id in stored_posts
-        else :
-            is_saved_for_later = False
-
-        return is_saved_for_later
+        return post_id in stored_posts if stored_posts else False
     
     def get(self, request, slug):
-        post = Post.objects.get(slug=slug)
-        
+        # Display a single post details
+        post = get_object_or_404(Post, slug=slug)
         context = {
             "post": post,
             "post_tags": post.tags.all(),
@@ -58,29 +44,27 @@ class SinglePostView(View):
         }
         return render(request, "blog/post-detail.html", context)
 
-        
     @method_decorator(login_required)
     def post(self, request, slug):
+        # Handle comment submission for a single post
         comment_form = CommentForm(request.POST)
-        post = Post.objects.get(slug=slug)
+        post = get_object_or_404(Post, slug=slug)
 
-        if request.user.is_authenticated and comment_form.is_valid():
+        if comment_form.is_valid():
             comment = comment_form.save(commit=False)
             comment.user = request.user
             comment.post = post
             comment.save()
+            return HttpResponseRedirect(reverse("blog:post-detail-page", args=[slug]))
 
-            return HttpResponseRedirect(reverse("post-detail-page", args=[slug]))
-
+        # Re-render the page with existing context and the invalid form
         context = {
             "post": post,
             "post_tags": post.tags.all(),
             "comment_form": comment_form,
             "comments": post.comments.all().order_by('-id'),
-            # "saved_for_later": self.is_stored_posts(request, post.id)
         }
         return render(request, "blog/post-detail.html", context)
-
 
 class ReadLaterView(LoginRequiredMixin, View):
     def get(self, request):
