0cc3103acb6606e6f36543af4c24e3b167789bba
Klurifixus <144474109+Klurifixus@users.noreply.github.com>
2023-12-14 10:03:57 +0100

    #13 path typo fix for users1

--- earnsforum/earnsforum/urls.py
+++ earnsforum/earnsforum/urls.py
@@ -24,7 +24,7 @@ urlpatterns = (
     [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls', namespace='blog')),
-    path('users/', include('users1.urls', namespace='users1')),
+    path('users1/', include('users1.urls', namespace='users1')),
     path('', StartingPageView.as_view(), name='home'),
     ]
     + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
