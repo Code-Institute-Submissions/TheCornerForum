c3b9549b0df87933eda17d5428e2002a96bb6783
Klurifixus <144474109+Klurifixus@users.noreply.github.com>
2023-12-14 09:32:58 +0100

    Add root URL configuration

--- earnsforum/earnsforum/urls.py
+++ earnsforum/earnsforum/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from blog.views import StartingPageView
 
 
 urlpatterns = (
@@ -24,6 +25,7 @@ urlpatterns = (
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls', namespace='blog')),
     path('users/', include('users1.urls', namespace='users1')),
+    path('', StartingPageView.as_view(), name='home'),
     ]
     + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
