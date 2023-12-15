bea8e3ab2aa52d887d9d68e5361d0c03a0d66a50
Klurifixus <144474109+Klurifixus@users.noreply.github.com>
2023-12-15 15:03:41 +0100

    #31 edited wsgi.py whith path, and run staticcollect for new css files

--- earnsforum/earnsforum/wsgi.py
+++ earnsforum/earnsforum/wsgi.py
@@ -8,8 +8,17 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
-
+from whitenoise import WhiteNoise
 from django.core.wsgi import get_wsgi_application
+
+
+from earnsforum import users1
+from earnsforum import blog
+
+users1 = WhiteNoise(users1, root='users1/static')
+blog = WhiteNoise(blog, root='blog/static')
+
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'earnsforum.settings')
 
