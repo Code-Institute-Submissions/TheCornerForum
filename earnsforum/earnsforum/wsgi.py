ecd01aa3edbb0b1b0bfde057d931004cca50bcfc
Klurifixus <144474109+Klurifixus@users.noreply.github.com>
2023-12-15 15:08:28 +0100

    #31 udated wsgi.py

--- earnsforum/earnsforum/wsgi.py
+++ earnsforum/earnsforum/wsgi.py
@@ -1,25 +1,11 @@
-"""
-WSGI config for earnsforum project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
-"""
-
 import os
-from whitenoise import WhiteNoise
 from django.core.wsgi import get_wsgi_application
-
-
-from earnsforum import users1
-from earnsforum import blog
-
-users1 = WhiteNoise(users1, root='users1/static')
-blog = WhiteNoise(blog, root='blog/static')
-
-
+from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'earnsforum.settings')
 
+# Get the WSGI application object for the Django project
 application = get_wsgi_application()
+
+# Wrap the entire Django application with WhiteNoise
+application = WhiteNoise(application)
