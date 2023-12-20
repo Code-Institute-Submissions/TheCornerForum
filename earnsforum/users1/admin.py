74ceb183fa4e1067f836ac9b1da79863ca9545d8
Klurifixus <144474109+Klurifixus@users.noreply.github.com>
2023-12-20 23:46:42 +0100

    #48 set up userprofile on users1 app in admin

--- earnsforum/users1/admin.py
+++ earnsforum/users1/admin.py
@@ -3,6 +3,6 @@ from .models import UserProfile
 
 @admin.register(UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
-    list_display = ('user', 'other_fields')
+    list_display = ('user')
     search_fields = ('user__username', 'user__email')
     list_filter = ('user__is_active',)
