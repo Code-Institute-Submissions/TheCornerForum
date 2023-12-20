97c6f0fba90047d5f33955a5105286235457c588
Klurifixus <144474109+Klurifixus@users.noreply.github.com>
2023-12-20 23:44:01 +0100

    #48 set up userprofile on users1 app in init,admin and updated signals.py

--- earnsforum/users1/admin.py
+++ earnsforum/users1/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import UserProfile
 
-# Register your models here.
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'other_fields')
+    search_fields = ('user__username', 'user__email')
+    list_filter = ('user__is_active',)
