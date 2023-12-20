1713545880944e393a642aebaa20d7fe6d46ac04
Klurifixus <144474109+Klurifixus@users.noreply.github.com>
2023-12-20 13:40:26 +0100

    FIX BUG #11

--- earnsforum/users1/apps.py
+++ earnsforum/users1/apps.py
@@ -1,8 +1,9 @@
 from django.apps import AppConfig
 
 
-class BlogConfig(AppConfig):
-    name = 'blog'
+class UserauthConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'users1'
 
     def ready(self):
-        import blog.signals
+        import users1.signals
