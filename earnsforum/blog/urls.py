78f73c24bd385d43dc29bb43a1e23d643b95b203
Klurifixus <144474109+Klurifixus@users.noreply.github.com>
2023-12-20 15:04:45 +0100

    FIX BUG #11

--- earnsforum/blog/urls.py
+++ earnsforum/blog/urls.py
@@ -12,7 +12,6 @@ urlpatterns = [
     path("read-later", views.ReadLaterView.as_view(), name="read-later"),
     path("cartoons/", views.CartoonView.as_view(), name="cartoons-page"),
     path("cartoons/<slug:slug>/", views.CartoonDetailView.as_view(), name="cartoon_detail"),
-    path('add_comment/<int:post_id>/', add_comment_to_post, name='add_comment'),
     path('edit_comment/<int:comment_id>/', edit_comment, name='edit_comment'),
     path('delete_comment/<int:comment_id>/', delete_comment, name='delete_comment'),
     path('save_post/<int:post_id>/', save_post, name='save_post'),
