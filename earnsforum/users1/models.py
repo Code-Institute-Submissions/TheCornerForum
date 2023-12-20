cab7086c2ed870e4bc910299360b9552ec515c0c
Klurifixus <144474109+Klurifixus@users.noreply.github.com>
2023-12-20 13:16:29 +0100

    FIX BUG #11 AttributeError at /blog/read-later

--- earnsforum/users1/models.py
+++ earnsforum/users1/models.py
@@ -3,21 +3,23 @@ from django.contrib.auth.models import User
 from django.apps import apps
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from blog.models import SavedContent
+
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    #saved_content = models.OneToOneField(SavedContent, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return self.user.username
+
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         UserProfile.objects.create(user=instance)
-        # Get the SavedContent model using apps.get_model
-        SavedContent = apps.get_model('blog', 'SavedContent')
-        # Check if SavedContent instance already exists for this user
-        saved_content, created = SavedContent.objects.get_or_create(user_profile=instance.userprofile)
-        if created:
-            instance.userprofile.saved_content = saved_content
-            instance.userprofile.save()
+        SavedContent = apps.get_model(
+            'blog', 'SavedContent')  # Use apps.get_model here
+        saved_content, _ = SavedContent.objects.get_or_create()
+        instance.userprofile.saved_content = saved_content
+        instance.userprofile.save()
