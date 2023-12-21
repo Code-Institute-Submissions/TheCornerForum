884e36718fae14aa8967a9a59da14b2c5e604b3d
Klurifixus <144474109+Klurifixus@users.noreply.github.com>
2023-12-21 14:31:52 +0100

    create a profile page for edit and delete account #49

--- earnsforum/users1/urls.py
+++ earnsforum/users1/urls.py
@@ -5,7 +5,7 @@ from django.contrib.auth import views as auth_views
 app_name = 'users1'
 
 urlpatterns = [
-    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
     path('signup/', views.user_signup, name='signup'),
     path('logout/', views.user_logout, name='logout'),
     path('edit_account/', views.edit_account, name='edit_account'),
