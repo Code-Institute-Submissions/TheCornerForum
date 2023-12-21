c5e571a7079d96dc1778996390554d0a645336a0
Klurifixus <144474109+Klurifixus@users.noreply.github.com>
2023-12-21 01:10:33 +0100

    installed dotenv for djnago local testing again .Session Data Management #42

--- earnsforum/earnsforum/settings.py
+++ earnsforum/earnsforum/settings.py
@@ -17,6 +17,8 @@ import django_heroku
 import dj_database_url
 import cloudinary
 import cloudinary_storage
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
