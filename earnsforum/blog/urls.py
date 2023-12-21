2bb6485bf109a7042dbb284c424baf5c761b4464
Klurifixus <144474109+Klurifixus@users.noreply.github.com>
2023-12-21 21:38:56 +0100

    Error Handling #41

--- earnsforum/blog/urls.py
+++ earnsforum/blog/urls.py
@@ -11,4 +11,10 @@ urlpatterns = [
     path("read-later", views.ReadLaterView.as_view(), name="read-later"),
     path("cartoons/", views.CartoonView.as_view(), name="cartoons-page"),
     path("cartoons/<slug:slug>/", views.CartoonDetailView.as_view(), name="cartoon_detail"),
+
+    path('add_comment_to_post/<int:comment_id>/', views.add_comment_to_post, name='add_comment_to_post'),
+    path('edit_comment/<int:comment_id>/', views.edit_comment, name='edit_comment'),
+    path('delete_comment/<int:comment_id>/', views.delete_comment, name='delete_comment'),
+    path('save_post/<int:post_id>/', views.save_post, name='save_post'),
 ]
+
