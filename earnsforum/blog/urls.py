d282be8945d9494a74fa94dfa16b4a5e46f14665
Klurifixus <144474109+Klurifixus@users.noreply.github.com>
2023-12-21 20:37:46 +0100

    #41 add all-posts back to urls becuz of 404 error

--- earnsforum/blog/urls.py
+++ earnsforum/blog/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path("", views.StartingPageView.as_view(), name="starting-page"),
     path('posts/', views.AllPostsView.as_view(), name='posts-page'),
     path("posts/<slug:slug>", views.SinglePostView.as_view(), name="post-detail-page"),
+    path('all-posts/', views.AllPostsView.as_view(), name='all-posts-page'),
     path("read-later", views.ReadLaterView.as_view(), name="read-later"),
     path("cartoons/", views.CartoonView.as_view(), name="cartoons-page"),
     path("cartoons/<slug:slug>/", views.CartoonDetailView.as_view(), name="cartoon_detail"),
