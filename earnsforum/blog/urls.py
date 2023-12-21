5ffb42cdf14119f0563346181cf45cd5d3d7b6c4
Klurifixus <144474109+Klurifixus@users.noreply.github.com>
2023-12-21 20:30:41 +0100

    Error Handling #41 make post be shown in all post

--- earnsforum/blog/urls.py
+++ earnsforum/blog/urls.py
@@ -5,7 +5,7 @@ app_name = 'blog'
 
 urlpatterns = [
     path("", views.StartingPageView.as_view(), name="starting-page"),
-    path('all-posts/', views.AllPostsView.as_view(), name='all-posts-page'),
+    path('posts/', views.AllPostsView.as_view(), name='posts-page'),
     path("posts/<slug:slug>", views.SinglePostView.as_view(), name="post-detail-page"),
     path("read-later", views.ReadLaterView.as_view(), name="read-later"),
     path("cartoons/", views.CartoonView.as_view(), name="cartoons-page"),
