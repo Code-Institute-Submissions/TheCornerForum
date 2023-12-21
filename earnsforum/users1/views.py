676fbb586233c32f6ab9290170d5bc59852937ec
Klurifixus <144474109+Klurifixus@users.noreply.github.com>
2023-12-21 10:56:24 +0100

    setup profile for edit and delete account  #49

--- earnsforum/users1/views.py
+++ earnsforum/users1/views.py
@@ -1,19 +1,14 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
-from .forms import UserCreationForm, LoginForm
+from .forms import UserCreationForm, LoginForm, UserProfileForm
 from django.contrib.auth.forms import UserChangeForm
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import logout
 from django.contrib import messages
-from django.utils import timezone
-from .models import DeletedAccountLog
-from .forms import UserProfileForm
-
+from .models import UserProfile, DeletedAccountLog
 
 # Home page (guide)
 def index(request):
     return render(request, 'blog/index.html')
-
 
 # signup page
 def user_signup(request):
@@ -24,83 +19,66 @@ def user_signup(request):
             return redirect('users1:login')
     else:
         form = UserCreationForm()
-    return render(request, 'signup.html', {'form': form})
+    return render(request, 'users1/signup.html', {'form': form})
 
 # login page
-
-
 def user_login(request):
-    next_page = request.GET.get('next') or request.POST.get(
-        'next') or 'starting-page'
-
+    next_page = request.GET.get('next', 'starting-page')
     if request.method == 'POST':
         form = LoginForm(request.POST)
         if form.is_valid():
             username = form.cleaned_data['username']
             password = form.cleaned_data['password']
-            user = authenticate(request, username=username, password=password)
+            user = authenticate(username=username, password=password)
             if user:
                 login(request, user)
                 return redirect(next_page)
     else:
         form = LoginForm()
-
-    return render(request, 'login.html', {'form': form, 'next': next_page})
-
+    return render(request, 'users1/login.html', {'form': form})
 
 @login_required
 def user_logout(request):
     logout(request)
     return redirect('starting-page')
 
-
+@login_required
 def edit_account(request):
     if request.method == 'POST':
         form = UserChangeForm(request.POST, instance=request.user)
         if form.is_valid():
             form.save()
-            # Redirect to a success page or profile page
-            return redirect('profile')
+            messages.success(request, "Account updated successfully.")
+            return redirect('users1:profile')
     else:
         form = UserChangeForm(instance=request.user)
-
-    return render(request, 'users1/profile.html', {'form': form})
-
+    return render(request, 'users1/edit_account.html', {'form': form})
 
 @login_required
 def delete_account(request):
     if request.method == 'POST':
-
-        profile = request.user.users1_profile
+        profile = request.user.userprofile
         profile.is_deleted = True
-        profile.deleted_at = timezone.now()
         profile.save()
-
-        # Log the deletion
-        DeletedAccountLog.objects.create(
-            user_id=request.user.id, deletion_date=timezone.now())
-
-        # Anonymize or modify other user data as necessary
-        # request.user.email = 'anonymized@example.com'
-        # request.user.username = 'anonymized_user_' + str(request.user.id)
-        # request.user.save()
-
+        DeletedAccountLog.objects.create(user_id=request.user.id)
         logout(request)
         messages.info(request, "Your account has been marked as deleted.")
-        # Replace 'home' with the name of your home page URL
         return redirect('starting-page')
-
-    return render(request, 'users1/profile.html')
+    return render(request, 'users1/delete_account.html')
 
 @login_required
 def update_profile(request):
     if request.method == 'POST':
-        form = UserProfileForm(request.POST, request.FILES, instance=request.user.users1_profile)
+        form = UserProfileForm(request.POST, request.FILES, instance=request.user.userprofile)
         if form.is_valid():
             form.save()
             messages.success(request, "Profile updated successfully.")
-            return redirect('profile')  # Replace with the URL name for the profile page
+            return redirect('users1:profile')
     else:
-        form = UserProfileForm(instance=request.user.users1_profile)
+        form = UserProfileForm(instance=request.user.userprofile)
 
-    return render(request, 'users1/profile.html', {'form': form})
+    return render(request, 'users1/profile.html', {'profile_form': form})
+
+@login_required
+def user_profile(request):
+    return render(request, 'users1/profile.html')
